29e2646bff4c7f83e0a9bd6f29b6a13430b44437
İrem Bağçıvan <230601013@bakircay.edu.tr>
2024-07-16 21:18:34 +0300

    update valid_parantheses

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -24,3 +24,41 @@ solution = Solution()
 print(solution.isValid("()"))       
 print(solution.isValid("(]"))       
 print(solution.isValid("()[]{}"))   
+
+# ilhanın çözümü
+
+# while True:
+#     metin = input("Metin giriniz: ")
+
+#     deger1 = True
+#     for i in range(0, len(metin)):
+#         if metin[i] == '(':
+#             if metin[i + 1] == ')':
+#                 deger1 = True
+
+#             else:
+#                 deger1 = False
+
+#     deger2 = True
+#     for i in range(0, len(metin)):
+#         if metin[i] == '[':
+#             if metin[i + 1] == ']':
+#                 deger2 = True
+
+#             else:
+#                 deger2 = False
+
+#     deger3 = True
+#     for i in range(0, len(metin)):
+#         if metin[i] == '{':
+#             if metin[i + 1] == '}':
+#                 deger3 = True
+
+#             else:
+#                 deger3 = False
+
+#     if deger1 == True and deger2 == True and deger3 == True:
+#         print("True")
+
+#     else:
+#         print("False")
